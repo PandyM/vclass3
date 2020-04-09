21674dd2a6ddeec24eb65bc2df5f729eaa02e446
Pandy muhamad <33719236+PandyM@users.noreply.github.com>
2020-04-09 17:44:29 +0700

    Create vclass.h

---
+++ vclass.h
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<iostream>
+#include<conio.h>
+#include<string>
+
+using namespace std;
+konversi{
+int main(int argc, char const *argv[])
+}
+
+ public:
+int nilai;
+
+
+cout<<"Nilai angka = ";
+cin >> nilai;
+
+
+if ((nilai >90)&&(nilai <100))
+ {
+ cout << "Nilai huruf adalah A" << endl;
+ }
+else if ((nilai >70)&&(nilai <89))
+{
+cout << "Nilai huruf adalah B" << endl;
+}
+else if ((nilai >45)&&(nilai <69))
+ {
+ cout<<"Nilai huruf adalah C" << endl;
+ }
+else if ((nilai >25)&&(nilai <44))
+{
+cout << "Nilai huruf adalah D" << endl;
+}
+else if ((nilai > 0)&& (nilai < 24))
+{
+cout <<  "Nilai huruf adalah E" << endl;
+}
+else
+{
+cout << " Nilai yang anda masukkan salah...! "<< endl ;
+}
+
+return 0;
+}
